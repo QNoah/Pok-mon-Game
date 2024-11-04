5c954807030d5880cb339c1451d6e12b1d34b9a0
Quinten <90308786+QNoah@users.noreply.github.com>
2024-11-04 14:51:44 +0100

    menu has been added for attacks, example type attacks are visible.

--- battle.py
+++ battle.py
@@ -27,6 +27,25 @@ def delay_print(s, delay=0.08):
     print()
 
 
+def attacks():
+    print("""
+1. Tackle
+2. Ember
+
+- Type back to go back.
+""")
+    attack = input().strip()
+    while True:
+        if attack == "1":
+            print("Used Tackle")
+            return
+        elif attack == "2":
+            print("Used Ember")
+            return "cpu"
+        elif attack == "back":
+            return "back" 
+
+
 def fight(id, myPokemon):
     battleSong.play(loops=-1)
     os.system("cls")
@@ -36,29 +55,135 @@ def fight(id, myPokemon):
     playSound("Pokemon Out", 0.3)
     delay_print(f"You chose {myPokemon.name}")
     while myPokemon.health > 0 and enemy.health > 0:
-        if turn == "player":
-            print("What is your next move?")
-            print("1. Escape (Go back the steps you rolled.)")
-            print(f"2. Fight (roll 0 > {myPokemon.attack})")
-            print(f"3. Use a item (work in project)")
-            while True:
-                try:
-                    _ = int(input())
-                    break
-                except ValueError:
-                    print("Not a valid option")
-                    continue
-            playSound("select", 1)
-            if _ == 1:
-                pass
-                # lose()
-            elif _ == 2:
-                damage = random.randint(0, myPokemon.attack)
-                if damage == 0:
+        battle_menu(turn, enemy, myPokemon)
+
+
+def battle_menu(turn, enemy, myPokemon):
+            if turn == "player":
+                print("What is your next move?")
+                print("1. Escape (Go back the steps you rolled.)")
+                print(f"2. Fight (roll 0 > {myPokemon.attack})")
+                print(f"3. Use a item (work in project)")
+                while True:
+                    try:
+                        _ = int(input())
+                        break
+                    except ValueError:
+                        print("Not a valid option")
+                        continue
+                playSound("select", 1)
+                if _ == 1:
+                    ...
+                    # lose()
+                elif _ == 2:
+                    if attacks() == "back":
+                        battle_menu(turn, enemy, myPokemon)
+                    else:
+                        if enemy.health <= 0:
+                            playSound("In-Battle Faint No Health", 1)
+                            delay_print(f'{enemy.name} fainted.')
+                            time.sleep(1)
+                            delay_print(f'{myPokemon.name} has leveled up!')
+                            myPokemon.levelUp()
+                            time.sleep(1)
+                            delay_print(f'{myPokemon.name} is now lvl {myPokemon.lvl}')
+                            # playSound()
+                            time.sleep(1)
+                            battleSong.stop(), victorySong.play()
+                            delay_print(f'{myPokemon.name} wins!')
+                            while True:
+                                _ = input("Continue? (y/n)").lower()
+                                if _ == "y":
+                                    playSound("select", 1)
+                                    victorySong.stop()
+                                    break
+                                elif _ == "n":
+                                    playSound("select", 1)
+                                    continue
+                                else:
+                                    continue
+                        else:
+                            delay_print(f'{enemy.name} is at {enemy.health}HP')
+                            time.sleep(1)
+                            battle_menu("cpu", enemy, myPokemon)
+                    # damage = random.randint(0, myPokemon.attack)
+                    # if damage == 0:
+                    #     os.system("cls")
+                    #     time.sleep(1)
+                    #     playSound("dodge", 1)
+                    #     delay_print(f'{myPokemon.name} used tackle but missed the attack!')
+                    #     time.sleep(1)
+                    # else:
+                    #     time.sleep(1)
+                    #     playSound("tackle", 1)
+                    #     time.sleep(2)
+                    #     playSound("Hit Normal Damage", 1)
+                    #     os.system("cls")
+                    #     delay_print(f'{myPokemon.name} did {damage} damage to {enemy.name}')
+                    #     enemy.health -= damage
+                    #     time.sleep(1)
+                    #     if enemy.health <= 0:
+                    #         playSound("In-Battle Faint No Health", 1)
+                    #         delay_print(f'{enemy.name} fainted.')
+                    #         time.sleep(1)
+                    #         delay_print(f'{myPokemon.name} has leveled up!')
+                    #         myPokemon.levelUp()
+                    #         time.sleep(1)
+                    #         delay_print(f'{myPokemon.name} is now lvl {myPokemon.lvl}')
+                    #         # playSound()
+                    #         time.sleep(1)
+                    #         battleSong.stop(), victorySong.play()
+                    #         delay_print(f'{myPokemon.name} wins!')
+                    #         while True:
+                    #             _ = input("Continue? (y/n)").lower()
+                    #             if _ == "y":
+                    #                 playSound("select", 1)
+                    #                 victorySong.stop()
+                    #                 break
+                    #             elif _ == "n":
+                    #                 playSound("select", 1)
+                    #                 continue
+                    #             else:
+                    #                 continue
+                    #     else:
+                    #         delay_print(f'{enemy.name} is at {enemy.health}HP')
+                    #         time.sleep(1)
+                elif _ == 3:
                     os.system("cls")
+                    item_not_selected = True
+                    while item_not_selected:
+                        with open('inventory.json') as inv:
+                            items = json.load(inv)
+                            for item in items['inventory']:
+                                print(f"Option: {item['id']}, item: {item['name']}, qty: {item['qty']}")
+                            time.sleep(1)
+                            delay_print('Choose your item:')
+                            try:
+                                item_choice = int(input())
+                            except ValueError:
+                                print("Wrong input")
+                        for item in items['inventory']:
+                            if item['id'] == item_choice and item['qty'] > 0 and item['id'] != 2:
+                                item['qty'] -= 1
+                                delay_print(f'You used {item["name"]}')
+                                if item['id'] == 1:
+                                    #hp increase
+                                    pass
+                                if item['id'] == 3:
+                                    pass
+                                    # roll another dice?
+                                if item['id'] == 4:
+                                    pass
+                                    # ability does 2x damage
+                        #3w elif       
+                turn = "cpu"
+            elif turn == "cpu":
+                damage = random.randint(0, enemy.attack)
+                if damage == 0:
                     time.sleep(1)
+                    os.system("cls")
                     playSound("dodge", 1)
-                    delay_print(f'{myPokemon.name} used tackle but missed the attack!')
+                    delay_print(f'{enemy.name} used tackle but missed the attack!')
                     time.sleep(1)
                 else:
                     time.sleep(1)
@@ -66,94 +191,18 @@ def fight(id, myPokemon):
                     time.sleep(2)
                     playSound("Hit Normal Damage", 1)
                     os.system("cls")
-                    delay_print(f'{myPokemon.name} did {damage} damage to {enemy.name}')
-                    enemy.health -= damage
-                    time.sleep(1)
-                    if enemy.health <= 0:
+                    delay_print(f'{enemy.name} did {damage} damage to {myPokemon.name}')
+                    myPokemon.health -= damage
+                    time.sleep(3)
+                    if myPokemon.health <= 0:
                         playSound("In-Battle Faint No Health", 1)
-                        delay_print(f'{enemy.name} fainted.')
-                        time.sleep(1)
-                        delay_print(f'{myPokemon.name} has leveled up!')
-                        myPokemon.levelUp()
-                        time.sleep(1)
-                        delay_print(f'{myPokemon.name} is now lvl {myPokemon.lvl}')
-                        # playSound()
-                        time.sleep(1)
-                        battleSong.stop(), victorySong.play()
-                        delay_print(f'{myPokemon.name} wins!')
-                        while True:
-                            _ = input("Continue? (y/n)").lower()
-                            if _ == "y":
-                                playSound("select", 1)
-                                victorySong.stop()
-                                break
-                            elif _ == "n":
-                                playSound("select", 1)
-                                continue
-                            else:
-                                continue
+                        delay_print(f'{myPokemon.name} fainted.')
+                        delay_print(f'{enemy.name} wins!')
                     else:
-                        delay_print(f'{enemy.name} is at {enemy.health}HP')
+                        delay_print(f'{myPokemon.name} is at {myPokemon.health}HP')
                         time.sleep(1)
-            elif _ == 3:
-                os.system("cls")
-                item_not_selected = True
-                while item_not_selected:
-                    with open('inventory.json') as inv:
-                        items = json.load(inv)
-                        for item in items['inventory']:
-                            print(f"Option: {item['id']}, item: {item['name']}, qty: {item['qty']}")
-                        time.sleep(1)
-                        delay_print('Choose your item:')
-                        try:
-                            item_choice = int(input())
-                        except ValueError:
-                            print("Wrong input")
-                    for item in items['inventory']:
-                        if item['id'] == item_choice and item['qty'] > 0 and item['id'] != 2:
-                            item['qty'] -= 1
-                            delay_print(f'You used {item["name"]}')
-                            if item['id'] == 1:
-                                #hp increase
-                                pass
-                            if item['id'] == 3:
-                                pass
-                                # roll another dice?
-                            if item['id'] == 4:
-                                pass
-                                # ability does 2x damage
-                       #3w elif  
-                            
-                            
-                        
-                    
-            turn = "cpu"
-        elif turn == "cpu":
-            damage = random.randint(0, enemy.attack)
-            if damage == 0:
-                time.sleep(1)
-                os.system("cls")
-                playSound("dodge", 1)
-                delay_print(f'{enemy.name} used tackle but missed the attack!')
-                time.sleep(1)
-            else:
-                time.sleep(1)
-                playSound("tackle", 1)
-                time.sleep(2)
-                playSound("Hit Normal Damage", 1)
-                os.system("cls")
-                delay_print(f'{enemy.name} did {damage} damage to {myPokemon.name}')
-                myPokemon.health -= damage
-                time.sleep(3)
-                if myPokemon.health <= 0:
-                    playSound("In-Battle Faint No Health", 1)
-                    delay_print(f'{myPokemon.name} fainted.')
-                    delay_print(f'{enemy.name} wins!')
-                else:
-                    delay_print(f'{myPokemon.name} is at {myPokemon.health}HP')
-                    time.sleep(1)
-            turn = "player"
+                turn = "player"
 
-# fight(2, myPokemon = Pokemon(id = 1,name = "Charmander", lvl = 1))
+fight(2, myPokemon = Pokemon(id = 1,name = "Charmander", lvl = 1))
 
 # keyboard.add_hotkey('1')
