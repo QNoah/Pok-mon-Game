89e618c178df07fcf4933f01c8d0a3981e4e01ec
Quinten <quinten.nagel01@gmail.com>
2024-11-07 14:37:36 +0100

    Finished trainer profile, made a quick start for the pokemon replace in profile.

--- test.py
+++ test.py
@@ -1,13 +1,8 @@
 from trainer import trainerProfile
 
 player = trainerProfile(name="Quinten")
-print(player.party)
 player.add_party({"id": 1, "name": "Charmander"})
-print(player.party)
-player.add_party({"id": 1, "name": "Charmander"})
-print(player.party)
-player.add_party({"id": 1, "name": "Charmander"})
-print(player.party)
+player.add_party({"id": 2, "name": "Squirtle"})
+player.add_party({"id": 3, "name": "Pook"})
 # It will ask here what to do since it is quite full
 player.add_party({"id": 1, "name": "Charmander"})
-print(player.party)
--- trainer.py
+++ trainer.py
@@ -1,28 +1,52 @@
+import os
+
 class trainerProfile:
     def __init__(self, name):
         self.name = name
-        self.party = []
+        self.party = {"party": []}
         self.gyms = 0
 # {"id": 1, "name": "Charmander"}
 
-    def add_party(self, pokemon):
-        if len(self.party) != 3:
-            self.party.append(pokemon)
-        elif len(self.party) == 3:
-            print("Sorry, but you're party is full")
-            _ = input("Want to release a Pokémon to the wild to have this new one? y/n ").lower()
+    def add_party(self, new_pokemon):
+        if len(self.party["party"]) != 3:
+            self.party["party"].append(new_pokemon)
+        elif len(self.party["party"]) == 3:
             while True:
-                if _ == "n":
-                    confirmation = input("Are you sure? You will lose your caught Pokémon! y/n ")
-                    if confirmation == "y":
-                        print("You caught a Pokémon and immediatly let it go.")
-                        break
-                    elif confirmation == "n":
-                        break
-                elif _ == "y":
-                    print("What Pokémon would you like to release?")
-                    for pokemons in self.party:
-                        print(pokemons)
-                        break
-                        
+                print("Sorry, but you're party is full")
+                _ = input("Want to release a Pokémon to the wild to have this new one? y/n ").lower()
+                while True:
+                    if _ == "n":
+                        confirmation = input("Are you sure? You will lose your caught Pokémon! y/n ")
+                        if confirmation == "y":
+                            print("You caught a Pokémon and immediatly let it go.")
+                            break
+                        elif confirmation == "n":
+                            os.system("cls")
+                            break
+                    elif _ == "y":
+                        os.system('cls')
+                        print("What Pokémon would you like to release?")
+                        for pokemon in self.party['party']:
+                            print(f"{pokemon['id']}. {pokemon['name']}")
+                        print()
+                        print("5. Go back")
+                        while True:
+                            try:
+                                _ = int(input().strip())
+                                if _ == 1:
+                                    print("hello world")
+                                elif _ == 2:
+                                    print("hello world")
+                                elif _ == 3:
+                                    print("hello world")
+                                elif _ == 5:
+                                    os.system("cls")
+                                    break
+                                else:
+                                    print("That is not a valid option!")
+                                    continue
+                            except ValueError:
+                                    print("That is not a valid option!")
+                                    continue
+                    break
                     # confirmation = 
